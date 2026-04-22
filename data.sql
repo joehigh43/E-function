0af4a6ac38fdc0b53667bca08778224a49a4f5ac
Yousef Okily <youssefokily@gmail.com>
2026-04-22 23:11:51 +0200

    edited database

--- data.sql
+++ data.sql
@@ -1,52 +1,64 @@
 create database ecommrece;
 use ecommrece ;
- 
- 
+/*drop database ecommrece; */
+
 create table users(
-UserID varchar(10) constraint PK_ID  primary key,
-fullName varchar(100),
-Email varchar(100) unique,
-passwordHash varchar(255),
-phone varchar(20),
+UserID int identity(1,1) constraint PK_ID  primary key not null,
+fullName varchar(100) not null,
+Email varchar(100) unique not null,
+passwordHash varchar(255) not null,
+phone varchar(20) not null,
 Address text,
-UserType varchar(15) constraint CHK_userstype check(UserType IN('trader','customer','admin')),
-createdAt Time default current_timestamp
+UserType varchar(15) constraint CHK_userstype check(UserType IN('customer','admin')),
+createdAt DateTime DEFAULT CURRENT_TIMESTAMP
 );
 select * from users;
 
-alter table users
-alter column UserID varchar(10) not null ;
-
 create table products(
-productID varchar(10) constraint PK_PID primary key,
-productName varchar(30),
+productID int identity(1,1) constraint PK_PID primary key,
+productName varchar(30) not null,
 description varchar(255),
-price money,
-wholePrice money,
-stockQuantity int,
+price money not null,
+stockQuantity int default 0,
 ImageURL varchar(500),
-catagory varchar(30),
-traderID varchar(10) constraint FK_TID foreign key references users(userID),
-createdAt Time default current_timestamp
+category varchar(30) not null,
+createdAt DateTime DEFAULT CURRENT_TIMESTAMP,
+adminID int constraint FK_AID foreign key references users(UserID)
 );
 select * from products;
 
 create table orders(
-orderID varchar(10) constraint PK_OID primary key,
-buyerID varchar(10) constraint FK_BID foreign key references users(userID),
+orderID int identity(1,1) constraint PK_OID primary key,
+buyerID int constraint FK_BID foreign key references users(userID),
 totalAmount int,
 paymentMethod varchar(15) constraint CHK_PM check(paymentMethod IN('cash','installement')),
 address text,
-createdAt Time default current_timestamp
+createdAt DateTime DEFAULT CURRENT_TIMESTAMP
 );
 select * from orders;
 
 create table orederItems(
-orderItemID varchar(10) constraint PK_OIID primary key,
-orderID varchar(10) foreign key references orders(orderID),
-productID varchar(10) foreign key references products(productID),
-Quantity int,
-price money,
+orderItemID int identity(1,1) constraint PK_OIID primary key,
+orderID int foreign key references orders(orderID),
+productID int foreign key references products(productID),
+Quantity int not null,
 totalPrice money
 );
 select * from orederItems;
+
+
+
+create table carts (
+    cartID int identity(1,1) constraint PK_CID primary key,
+    customerID int foreign key references users(UserID),
+    CreatedAt DateTime DEFAULT CURRENT_TIMESTAMP
+);
+select * from carts;
+
+create table cartItems (
+    CartItemID int identity(1,1) primary key,
+    CartID int foreign key references carts(CartID),
+    ProductID int foreign key references products(ProductID),
+    Quantity INt
+);
+select * from cartItems;
